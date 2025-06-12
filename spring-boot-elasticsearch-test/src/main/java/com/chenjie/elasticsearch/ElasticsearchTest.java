a7b1ab09465b62550c02998e6cc24c464c3aaad1
chenjie <253248079@qq.com>
2025-06-12 16:58:02 +0800

    ES的测试方法

--- spring-boot-elasticsearch-test/src/main/java/com/chenjie/elasticsearch/ElasticsearchTest.java
+++ spring-boot-elasticsearch-test/src/main/java/com/chenjie/elasticsearch/ElasticsearchTest.java
@@ -2,6 +2,8 @@ package com.chenjie.elasticsearch;
 
 import com.chenjie.elasticsearch.vo.AnnouncementVo;
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.core.GetResponse;
+import co.elastic.clients.elasticsearch.core.IndexResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,15 +21,59 @@ public class ElasticsearchTest {
     @Autowired
     private ElasticsearchClient client;
 
-    private static final AnnouncementVo searchContent = new AnnouncementVo();
-
-    static {
-        searchContent.setType(2);
-        searchContent.setAnnouncementcontent("茅台");
-    }
+    private static final String INDEX_NAME = "test";
+    private static final String DOCUMENT_ID = "1";
 
     @Test
     public void testElasticsearch() throws IOException {
+        // 尝试获取文档
+        GetResponse<Object> response = client.get(g -> g
+            .index(INDEX_NAME)
+            .id(DOCUMENT_ID),
+            Object.class
+        );
 
+        if (response.found()) {
+            log.info("文档已存在: {}", response.source());
+        } else {
+            // 创建新文档
+            IndexResponse indexResponse = client.index(i -> i
+                .index(INDEX_NAME)
+                .id(DOCUMENT_ID)
+                .document(new TestDocument("test", "This is a test document"))
+            );
+            
+            log.info("文档创建成功: {}", indexResponse.result());
+        }
+    }
+}
+
+// 测试文档类
+class TestDocument {
+    private String title;
+    private String content;
+
+    public TestDocument() {
+    }
+
+    public TestDocument(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 }
